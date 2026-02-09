336d7dcc61a4f535c56520b1fc6108edb19ddb0f
Jia Jia <angersax@sina.com>
2026-02-09 14:43:04 +0800

    refactor(build): rename exported plugin jar to gerrit-oauth-provider.jar for gerrit 3.4

--- 3.4/build.sh
+++ 3.4/build.sh
@@ -2,5 +2,5 @@
 
 docker build --no-cache -t gerrit-plugins-oauth:3.4 .
 docker run -it -d --name gerrit-plugins-oauth gerrit-plugins-oauth:3.4
-docker cp gerrit-plugins-oauth:/workspace/output/oauth.jar .
+docker cp gerrit-plugins-oauth:/workspace/output/gerrit-oauth-provider.jar .
 docker rm -f gerrit-plugins-oauth
