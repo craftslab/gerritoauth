35f3a378eb3c7d892333fbb1976c4516b4427cde
Jia Jia <angersax@sina.com>
2026-02-09 13:10:23 +0800

    fix(build): fix incorrect plugin name for gerrit 2.13

--- 2.13/build.sh
+++ 2.13/build.sh
@@ -2,5 +2,5 @@
 
 docker build --no-cache -t gerrit-plugins-oauth:2.13 .
 docker run -it -d --name gerrit-plugins-oauth gerrit-plugins-oauth:2.13
-docker cp gerrit-plugins-oauth:/workspace/output/oauth.jar .
+docker cp gerrit-plugins-oauth:/workspace/output/gerrit-oauth-provider.jar .
 docker rm -f gerrit-plugins-oauth
